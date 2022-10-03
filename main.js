ba86c332619b1b4aa8b4f6f7c8feb1b005a3086f
Srikaushal <82885264+Srikaushal@users.noreply.github.com>
2022-10-03 16:56:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function home_page(){
+    window.location="index.html"
+}
+
+function My_routine(){
+    window.location="routine.html"
+}
+
+function learnings(){
+    window.location="learnings.html"
+}
+
+function my_family(){
+    window.location="family.html"
+}   
